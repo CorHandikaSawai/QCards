5dde9a27d7a630c7bb187d8ea6c9e4dc4034a138
Cor Sawai <handika033@gmail.com>
2022-10-25 20:09:57 -0500

    add new screen for creating cards.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:free_quizme/screens/create_card_screen.dart';
+import 'package:free_quizme/screens/quiz_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Free Quizme',
       theme: ThemeData.dark(),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CreateCardsScreen(),
     );
   }
 }
@@ -30,64 +32,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Title of the Subject"),
-        centerTitle: true,
-      ),
-      body: SafeArea(
-        minimum: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              margin: const EdgeInsets.all(5.0),
-              height: screenSize.height * 0.4,
-              color: Colors.black12,
-              child: const Center(
-                child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    'Consectetur excepteur culpa sint quis aute anim id. Duis nulla excepteur officia mollit proident Lorem laboris consequat ad tempor aute cupidatat qui est. Irure eiusmod occaecat exercitation laborum eiusmod deserunt cillum adipisicing.',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontSize: 24,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              height: screenSize.height * 0.5,
-              child: GridView.count(
-                crossAxisCount: 2,
-                children: const [
-                  Answer(),
-                  Answer(),
-                  Answer(),
-                  Answer(),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Answer extends StatelessWidget {
-  const Answer({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(5),
-      child: const Center(child: Text('Answer A')),
-      color: Colors.black12,
+      child: Text('I do not know what to put here yet'),
     );
   }
 }
